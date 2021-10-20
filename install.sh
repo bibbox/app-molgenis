c867862cb71a963069947f1475bd54702c170f93
Emilian <emilian.jung+github@gmail.com>
2021-10-20 16:25:26 +0200

    Updated Readme and install.sh
    in accordance to reviewer comment

--- install.sh
+++ install.sh
@@ -2,7 +2,12 @@
 mkdir -p data/var/lib/postgresql/data
 mkdir -p data/usr/share/elasticsearch/data
 mkdir -p data/home/molgenis
+mkdir -p data/minio/data
 
 cp backend.conf data/backend.conf
+
 chmod -R 777 data
-docker-compose up -d
+
+docker network create bibbox-default-network
+
+docker-compose up
